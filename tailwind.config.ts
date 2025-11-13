d5e98822b58ea6ae87f5774d5934bc0c576d009a
iiMaximus <38739023+iiMaximus@users.noreply.github.com>
2025-11-13 02:43:49 +0100

    Update Tailwind config for content paths and dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class",
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
